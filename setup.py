d323ad02de6603700794f5dacec4e19d99db6e92
heshara2000 <hesharadananjanee@gmail.com>
2025-08-29 22:38:47 +0530

    web app cpmpleted

---
+++ setup.py
@@ -0,0 +1,11 @@
+from seruptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Booktiar Ahmed Bappy",
+    author_email="hesharadananjanee@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
